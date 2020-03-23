5d33a09c47bd69712cd3bf607e71ceb7a0f27354
Crushxl <62500850+Crushxl@users.noreply.github.com>
2020-03-23 10:16:44 +0800

    Aill

--- modules/Core/queries/alerts.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!$user->isLoggedIn()){
-	die(json_encode(array("value" => 0)));
-}
-
-$alerts = Alert::getAlerts($user->data()->id);
-
-echo json_encode(array("value" => count($alerts), "alerts" => $alerts));
--- modules/Core/queries/pms.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!$user->isLoggedIn()){
-	die(json_encode(array("value" => 0)));
-}
-
-$pms = Alert::getPMs($user->data()->id);
-
-echo json_encode(array("value" => count($pms), "pms" => $pms));
